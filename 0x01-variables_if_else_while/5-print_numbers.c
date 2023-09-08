373610b1d4f7ee8df6359d14d78ba2f2c9efeb0b
a15h4 <aishasharif@gmail.com>
2023-09-08 06:32:09 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	int z;
+
+	for (z = 0; z < 0; z++)
+	{
+	putchar(z + '0');
+	}
+	putchar('\n');
+	return (0);
+}
