b609395a89f249953da52c64a4a0cb9e315616f4
a15h4 <aishasharif@gmail.com>
2023-09-08 08:57:09 -0700

    printcomb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,14 +10,15 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num <= 9; num++)
+	for (num = 0; num <= 57; num++)
 	{
-		putchar((num % 10) + '0');
-		if (num == 9)
-			continue;
-
-		putchar(',');
 		putchar(num);
+		if (num == 57)
+		{
+			continue;
+		}
+		putchar(',');
+		putchar(' ');
 	}
 
 	putchar('\n');
