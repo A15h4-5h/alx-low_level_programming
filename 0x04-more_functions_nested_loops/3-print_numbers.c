1003f5fc8256e6a6179cc3304711eef5cda1aac2
a15h4 <aishasharif@gmail.com>
2023-09-14 10:09:57 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * print_numbers - prints numbers
+  * Return: void
+  */
+
+void print_most_numbers(void)
+{
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		_putchar(j + '0');
+	}
+		_putchar('\n');
+}
