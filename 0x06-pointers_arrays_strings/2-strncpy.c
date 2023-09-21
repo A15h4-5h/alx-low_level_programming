a9edbd7c6710fecb7092626c62eaec077fe6158e
a15h4 <aishasharif@gmail.com>
2023-09-21 10:46:15 -0700

    2-strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * _strncpy - name of the function
+  * @dest: parameter 1
+  * @src: parameter 2
+  * @n: parameter 3
+  * Return: dest
+  */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int l;
+
+	for (l = 0; l < n && src[l] != '\0'; l++)
+	{
+		while (l < n)
+		{
+			dest[l] = '\0';
+			l++;
+		}
+	}
+		return (dest);
+}
